4378800701e29da05abfac4e4665e321bdc2475c
Hugo Massing <hugomassing@users.noreply.github.com>
2018-09-27 09:50:31 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import 'react-sound'
 
 import App from './components/App'
 import store from './store'
